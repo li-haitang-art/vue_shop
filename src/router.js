28ef9b37d9645bc9d6e7277c821cc6974bf848f2
li-haitang <1499675881@qq.com>
2020-12-22 18:40:57 +0800

    完成了登录操作

--- src/router.js
+++ src/router.js
@@ -1,25 +1,37 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Login from './components/login.vue'
+import './assets/css/global.css'
+import Home from './components/Home.vue'
 
 Vue.use(Router)
 
-export default new Router({
+const router =  new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home
+      path : '/',
+      redirect : '/login'
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path:'/login',
+      component:Login
+    },{
+      path : '/home',
+      component : Home
     }
   ]
 })
+router.beforeEach((to,from ,next)=>{
+  //to 将要访问的路径
+  //from 代表从哪个路径跳转而来
+  //next 是一个函数，表示放行
+  // next() 放行  next('/login')强制跳转
+  if(to.path === '/login') return next();
+  //获取const 
+  const tokenStr = window.sessionStorage.getItem('token')
+  if(!tokenStr) return next('/login');
+  next()
+})
+export default router
